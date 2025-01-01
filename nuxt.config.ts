37db5329524c14e90205ab340b0d1b0258027883
charlieop <spchanae@connect.ust.hk>
2025-01-01 16:07:17 +0800

    added title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,12 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     baseURL: "/2025YearSummary-Preview/",
+    head: {
+      title: "Triple UNI 2024 年度总结",
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "https://tripleuni.com/img/logo-512.461b29bd.png" },
+      ]
+    }
   },
 });
